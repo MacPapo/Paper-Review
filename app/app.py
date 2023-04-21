881ea00fe6cae017248da4eefcfe74bbb5f7f18a
MacPapo <jacopocostantini32@gmail.com>
2023-04-21 22:21:26 +0200

    Ready for testing

--- app/app.py
+++ app/app.py
@@ -3,8 +3,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+def home_page():
+    return "<p>Hello from Home page!</p>"
+
+@app.route("/hello")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Hello from hello page!</p>"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
