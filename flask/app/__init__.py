810c14393bb34457c341151b7448d8cd5e9e417b
MacPapo <jacopocostantini32@gmail.com>
2023-04-26 17:37:26 +0200

    Added Flask Debug Toolbar

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -1,5 +1,10 @@
 from flask import Flask
+from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'bucami'
+app.debug = True
+
+toolbar = DebugToolbarExtension(app)
 
 from app import routes
