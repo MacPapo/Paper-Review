86053384a1b7223bb989224fdc0eb42fe761abe7
MacPapo <jacopocostantini32@gmail.com>
2023-04-26 18:14:33 +0200

    Added base db connection

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -1,10 +1,17 @@
 from flask import Flask
 from flask_debugtoolbar import DebugToolbarExtension
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'bucami'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://moonphase:eclipse@db:5432/paper_review'
 app.debug = True
 
 toolbar = DebugToolbarExtension(app)
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 from app import routes
