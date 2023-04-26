834c7b3d3088cc5886ecd34c7423ba2c59aabab2
MacPapo <jacopocostantini32@gmail.com>
2023-04-26 18:06:50 +0200

    Login handling

--- flask/app/routes.py
+++ flask/app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.modules.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -10,3 +11,12 @@ def index():
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
