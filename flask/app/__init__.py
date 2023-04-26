810d84185722e6d8709c20300c8e9bbe1aaf484e
MacPapo <jacopocostantini32@gmail.com>
2023-04-26 19:16:53 +0200

    Added login and registration page

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -2,12 +2,16 @@ from flask import Flask
 from flask_debugtoolbar import DebugToolbarExtension
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'bucami'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://moonphase:eclipse@db:5432/paper_review'
 app.debug = True
+
+login = LoginManager(app)
+login.login_view = 'login'
 
 toolbar = DebugToolbarExtension(app)
 
