f330a55ef5659f2d2d67f3824d36b78a8ad5bd5f
MrZhouDawei <890643@stud.unive.it>
2023-05-02 15:02:51 +0200

    Code refactor

--- flask/app/modules/forms.py
+++ flask/app/modules/forms.py
@@ -3,7 +3,7 @@ from flask_wtf import FlaskForm
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Length
 from wtform_address import CountrySelectField
 from app.models import User
-from datetime import date,datetime
+from datetime import date, datetime
 from wtforms_components import DateRange
 from wtforms import (
     StringField,
@@ -15,43 +15,122 @@ from wtforms import (
     MultipleFileField,
 )
 
+
 class LoginForm(FlaskForm):
-    username = StringField("Username", validators=[DataRequired()],
-                           render_kw={"class":"shadow-sm form-control","placeholder":"Username","size":"32"})
-    password = PasswordField("Password", validators=[DataRequired()],
-                           render_kw={"class":"shadow-sm form-control","placeholder":"Password","size":"32"})
+    username = StringField(
+        "Username",
+        validators=[DataRequired()],
+        render_kw={
+            "class": "shadow-sm form-control",
+            "placeholder": "Username",
+            "size": "32",
+        },
+    )
+    password = PasswordField(
+        "Password",
+        validators=[DataRequired()],
+        render_kw={
+            "class": "shadow-sm form-control",
+            "placeholder": "Password",
+            "size": "32",
+        },
+    )
     remember_me = BooleanField("Remember Me")
-    submit = SubmitField("Sign In",render_kw={"class":"btn btn-primary"})
+    submit = SubmitField("Sign In", render_kw={"class": "btn btn-primary"})
 
 
 class RegistrationForm(FlaskForm):
     today = date.today()
-    max_date = datetime.strptime(str(today.day) + '/'+ str(today.month) + '/'+ str(today.year - 18),'%d/%m/%Y').date()
-    min_date = datetime.strptime(str(today.day) + '/'+ str(today.month) + '/'+ str(today.year - 100),'%d/%m/%Y').date()
-    uid = StringField("Username", validators=[DataRequired(), Length(min=16, max=16)],
-                      render_kw={"class":"shadow-sm form-control","size":16,"placeholder":"User ID"})
-    username = StringField("User Name", validators=[DataRequired(), Length(min=6, max=32)],
-                      render_kw={"class":"shadow-sm form-control","size":32,"placeholder":"Username"})
-    firstname = StringField("First Name", validators=[DataRequired(), Length(min=1, max=32)],
-                      render_kw={"placeholder": "First Name","class":"shadow-sm form-control","size":32})
-    lastname = StringField("Last Name", validators=[DataRequired(), Length(min=1, max=64)],
-                      render_kw={"class":"shadow-sm form-control","size":64,"placeholder":"Last Name"})
-    birthdate = DateField("Birth Date",validators=[DataRequired(),DateRange(max=max_date,min=min_date)],
-                      render_kw={"class":"shadow-sm form-control"})
-    email = StringField("Email", validators=[DataRequired(), Email()],
-                      render_kw={"class":"shadow-sm form-control","placeholder":"Email","size":64})
-    sex = RadioField("Sex",choices=[('M','M'),('F','F'),('Other','Other')],validators=[DataRequired()]
-                      )
-    nationality = CountrySelectField(default='IT',
-                      render_kw={"class":"shadow-sm form-control"})
-    phone = StringField("Phone", validators=[DataRequired()],
-                      render_kw={"class":"shadow-sm form-control"})
-    password = PasswordField("Password", validators=[DataRequired()],
-                      render_kw={"class":"shadow-sm form-control","placeholder":"Password","size":32})
+    max_date = datetime.strptime(
+        str(today.day) + "/" + str(today.month) + "/" + str(today.year - 18), "%d/%m/%Y"
+    ).date()
+    min_date = datetime.strptime(
+        str(today.day) + "/" + str(today.month) + "/" + str(today.year - 100),
+        "%d/%m/%Y",
+    ).date()
+
+    uid = StringField(
+        "Username",
+        validators=[DataRequired(), Length(min=16, max=16)],
+        render_kw={
+            "class": "shadow-sm form-control",
+            "size": 16,
+            "placeholder": "User ID",
+        },
+    )
+    username = StringField(
+        "User Name",
+        validators=[DataRequired(), Length(min=6, max=32)],
+        render_kw={
+            "class": "shadow-sm form-control",
+            "size": 32,
+            "placeholder": "Username",
+        },
+    )
+    firstname = StringField(
+        "First Name",
+        validators=[DataRequired(), Length(min=1, max=32)],
+        render_kw={
+            "placeholder": "First Name",
+            "class": "shadow-sm form-control",
+            "size": 32,
+        },
+    )
+    lastname = StringField(
+        "Last Name",
+        validators=[DataRequired(), Length(min=1, max=64)],
+        render_kw={
+            "class": "shadow-sm form-control",
+            "size": 64,
+            "placeholder": "Last Name",
+        },
+    )
+    birthdate = DateField(
+        "Birth Date",
+        validators=[DataRequired(), DateRange(max=max_date, min=min_date)],
+        render_kw={"class": "shadow-sm form-control"},
+    )
+    email = StringField(
+        "Email",
+        validators=[DataRequired(), Email()],
+        render_kw={
+            "class": "shadow-sm form-control",
+            "placeholder": "Email",
+            "size": 64,
+        },
+    )
+    sex = RadioField(
+        "Sex",
+        choices=[("M", "M"), ("F", "F"), ("Other", "Other")],
+        validators=[DataRequired()],
+    )
+    nationality = CountrySelectField(
+        default="IT", render_kw={"class": "shadow-sm form-control"}
+    )
+    phone = StringField(
+        "Phone",
+        validators=[DataRequired()],
+        render_kw={"class": "shadow-sm form-control"},
+    )
+    password = PasswordField(
+        "Password",
+        validators=[DataRequired()],
+        render_kw={
+            "class": "shadow-sm form-control",
+            "placeholder": "Password",
+            "size": 32,
+        },
+    )
     password2 = PasswordField(
-        "Repeat Password", validators=[DataRequired(), EqualTo("password")],
-                      render_kw={"placeholder":"Repeat Password","size":32,"class":"shadow-sm form-control"})
-    submit = SubmitField("Register",render_kw={"class":"btn btn-primary"})
+        "Repeat Password",
+        validators=[DataRequired(), EqualTo("password")],
+        render_kw={
+            "placeholder": "Repeat Password",
+            "size": 32,
+            "class": "shadow-sm form-control",
+        },
+    )
+    submit = SubmitField("Register", render_kw={"class": "btn btn-primary"})
 
     def validate_uid(self, uid):
         user = User.query.filter_by(uid=uid.data).first()
@@ -74,8 +153,7 @@ class RegistrationForm(FlaskForm):
             if not phonenumbers.is_valid_number(p):
                 raise ValueError()
         except (phonenumbers.phonenumberutil.NumberParseException, ValueError):
-            raise ValidationError('Invalid phone number')
-
+            raise ValidationError("Invalid phone number")
 
 
 class UploadForm(FlaskForm):
